3807d25af430758e67a5bb0d721385f3e01d67f5
maximistic <110153672+maximistic@users.noreply.github.com>
2024-07-12 12:40:54 +0530

    init

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./app.css"
+import Header from './components/header/header';
+
+function App() {
+  return (
+    <>
+    <Header/>
+    </>
+  );
+}
+
+export default App;
