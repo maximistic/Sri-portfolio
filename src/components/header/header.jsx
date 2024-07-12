6c3b3a3b382802aa30d9a5746f5426134050af25
maximistic <110153672+maximistic@users.noreply.github.com>
2024-07-12 15:42:09 +0530

    header update

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -8,44 +8,46 @@ const Header = () => {
             <nav className="nav container">
                 <a href="index.html" className="nav__logo">Sri</a>
                 
-                <div className={Toggle ? "nav_menu show-menu": "nav_menu"}>
-                        <ul className="nav__list grid">
-                            <li className="nav__item">
-                                <a href="#home" className="nav__link active-link">
-                                    <i className="uil uil-estate nav__icon"></i> Home
-                                </a>
-                            </li>
-                            <li className="nav__item">
-                                <a href="#about" className="nav__link">
-                                    <i className="uil uil-user nav__icon"></i> About
-                                </a>
-                            </li>
-                            <li className="nav__item">
-                                <a href="#skills" className="nav__link">
-                                    <i className="uil uil-file-alt nav__icon"></i> Skills
-                                </a>
-                            </li>
-                            <li className="nav__item">
-                                <a href="#services" className="nav__link">
-                                    <i className="uil uil-briefcase-alt nav__icon"></i> Services
-                                </a>
-                            </li>
-                            <li className="nav__item">
-                                <a href="#portfolio" className="nav__link">
-                                    <i className="uil uil-scenery nav__icon"></i> Portfolio
-                                </a>
-                            </li>
-                            <li className="nav__item">
-                                <a href="#contact" className="nav__link">
-                                    <i className="uil uil-message nav__icon"></i> Contact
-                                </a>
-                            </li>
-                        </ul>
+                <div className={Toggle ? "nav__menu show-menu": "nav__menu"}>
+                    <ul className="nav__list grid">
+                        <li className="nav__item">
+                            <a href="#home" className="nav__link active-link">
+                                <i className="uil uil-estate nav__icon"></i> Home
+                            </a>
+                        </li>
+                        <li className="nav__item">
+                            <a href="#about" className="nav__link">
+                                <i className="uil uil-user nav__icon"></i> About
+                            </a>
+                        </li>
+                        <li className="nav__item">
+                            <a href="#skills" className="nav__link">
+                                <i className="uil uil-file-alt nav__icon"></i> Skills
+                            </a>
+                        </li>
+                        <li className="nav__item">
+                            <a href="#services" className="nav__link">
+                                <i className="uil uil-briefcase-alt nav__icon"></i> Services
+                            </a>
+                        </li>
+                        <li className="nav__item">
+                            <a href="#portfolio" className="nav__link">
+                                <i className="uil uil-scenery nav__icon"></i> Portfolio
+                            </a>
+                        </li>
+                        <li className="nav__item">
+                            <a href="#contact" className="nav__link">
+                                <i className="uil uil-message nav__icon"></i> Contact
+                            </a>
+                        </li>
+                    </ul>
 
-                        <i class="uil uil-times nav__close"></i>
+                    <i className="uil uil-times nav__close" onClick={() => showMenu(!Toggle)}></i>
                 </div>
-                <div className="nav__toggle" onClick={() => showMenu(!Toggle)}></div>
-                <i class="uil uil-apps"></i>
+                
+                <div className="nav__toggle" onClick={() => showMenu(!Toggle)}>
+                    <i className="uil uil-apps"></i>
+                </div>
             </nav>
         </header>
     )
