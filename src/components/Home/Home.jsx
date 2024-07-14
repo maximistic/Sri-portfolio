424bfcdb3888275ba0c64b40ad2900999e255711
maximistic <110153672+maximistic@users.noreply.github.com>
2024-07-14 15:01:57 +0530

    create data

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,14 +1,17 @@
 import React from "react";
-import "./home.css"
-import social from './social'
+import "./Home.css";
+import Social from './social';
+import Data from './Data';
 
 const Home = () => {
     return(
         <section className="home section" id="home">
             <div className="home__container container grid">
                 <div className="home_content grid">
-                    <social/>
+                    <Social />
                     
+                    <div className="home__img"></div>
+                    <Data />
                 </div> 
             </div>
         </section>
