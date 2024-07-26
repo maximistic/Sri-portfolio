8d9b61dd4d69052bd11bebea6c35fdc1fe5ed10f
maximistic <110153672+maximistic@users.noreply.github.com>
2024-07-26 16:28:26 +0530

    front

---
+++ src/components/FrontPage/FrontPage.jsx
@@ -0,0 +1,50 @@
+import React, { useEffect } from 'react';
+import { gsap } from 'gsap';
+import './FrontPage.css';  // Move styles to this file
+
+function FrontPage({ onAnimationComplete }) {
+  useEffect(() => {
+    gsap.fromTo(
+      ".loading-page",
+      { opacity: 1 },
+      {
+        opacity: 0,
+        display: "none",
+        duration: 1.5,
+        delay: 3.5,
+        onComplete: onAnimationComplete
+      }
+    );
+    
+    gsap.fromTo(
+      ".logo-name-front",
+      {
+        y: 50,
+        opacity: 0,
+      },
+      {
+        y: 0,
+        opacity: 1,
+        duration: 2,
+        delay: 0.5,
+      }
+    );
+  }, [onAnimationComplete]);
+
+  return (
+    <div className="container-front">
+      <p>Welcome to my world of Ideas</p>
+      <div className="loading-page">
+        <svg id="svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+          <path d="M94.1 315.1c0 25.9-21.2 47.1-47.1 47.1S0 341 0 315.1c0-25.9 21.2-47.1 47.1-47.1h47.1v47.1zm23.7 0c0-25.9 21.2-47.1 47.1-47.1s47.1 21.2 47.1 47.1v117.8c0 25.9-21.2 47.1-47.1 47.1s-47.1-21.2-47.1-47.1V315.1zm47.1-189c-25.9 0-47.1-21.2-47.1-47.1S139 32 164.9 32s47.1 21.2 47.1 47.1v47.1H164.9zm0 23.7c25.9 0 47.1 21.2 47.1 47.1s-21.2 47.1-47.1 47.1H47.1C21.2 244 0 222.8 0 196.9s21.2-47.1 47.1-47.1H164.9zm189 47.1c0-25.9 21.2-47.1 47.1-47.1 25.9 0 47.1 21.2 47.1 47.1s-21.2 47.1-47.1 47.1h-47.1V196.9zm-23.7 0c0 25.9-21.2 47.1-47.1 47.1-25.9 0-47.1-21.2-47.1-47.1V79.1c0-25.9 21.2-47.1 47.1-47.1 25.9 0 47.1 21.2 47.1 47.1V196.9zM283.1 385.9c25.9 0 47.1 21.2 47.1 47.1 0 25.9-21.2 47.1-47.1 47.1-25.9 0-47.1-21.2-47.1-47.1v-47.1h47.1zm0-23.7c-25.9 0-47.1-21.2-47.1-47.1 0-25.9 21.2-47.1 47.1-47.1h117.8c25.9 0 47.1 21.2 47.1 47.1 0 25.9-21.2 47.1-47.1 47.1H283.1z"/>
+        </svg>
+
+        <div className="name-container-front">
+          <div className="logo-name-front">MAXIMISTIC</div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default FrontPage;
